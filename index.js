6a0aa2a2c2ec60976e8c787dc82a6b1d77492881
Juan Barrera <juanpablo.is@outlook.com>
2020-08-23 13:45:55 -0500

    Create button Random for value table distribution.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var popup = document.getElementById('popup');
 var btnJugar = document.querySelector('#firstPnl h1');
 var btnGame = document.getElementById('submitGame');
+var btnRandom = document.getElementById('submitRandom');
 var valuesGame = new Object;
 
+//Call process that create table random
+processRandomTable();
 
+//Event for btn 'Jugar', display popup table
 btnJugar.addEventListener('click', function () {
     popup.style.display = 'flex';
 });
 
+//Evento for windown, in-display popup table with escape
 window.addEventListener('keydown', function (e) {
     if (e.key === 'Escape') {
         popup.style.display = 'none';
     }
+});
+
+//Event for btn 'Random' that call process random table
+btnRandom.addEventListener('click', function (e) {
+    e.preventDefault();
+    processRandomTable();
 });
 
 btnGame.addEventListener('click', function (e) {
@@ -32,3 +43,18 @@ btnGame.addEventListener('click', function (e) {
     localStorage.setItem('game', JSON.stringify(valuesGame));
     window.open('gaming.html', '_self');
 });
+
+//Function generate random for table distribution
+function processRandomTable() {
+    randomNumber(document.querySelectorAll('.clm-1'), 5);
+    randomNumber(document.querySelectorAll('.clm-2'), 4);
+    randomNumber(document.querySelectorAll('.clm-3'), 3);
+    randomNumber(document.querySelectorAll('.clm-4'), 2);
+}
+
+//For one column set value random range
+function randomNumber(column, numMax) {
+    for (let i = 0; i < column.length; i++) {
+        column[i].value = Math.floor(Math.random() * numMax + 1);
+    };
+}
