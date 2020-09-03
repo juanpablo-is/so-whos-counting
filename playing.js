353fb5bd3835666ea7b7e566d82de618b960cdf6
Juan Barrera <juanpablo.is@outlook.com>
2020-09-03 10:36:46 -0500

    Fix setTimeout.

--- playing.js
+++ playing.js
@@ -28,80 +28,77 @@ generateValues();
 async function generateValues() {
   for (let i = 0; i < values.cycle; i++) {
 
-    //Call function process simulate
-    processSimulate();
+    setTimeout(() => {
+      //Call function process simulate
+      processSimulate();
 
-    //Print value for User
-    let generate = `${fieldsUser[0]}${fieldsUser[1]}${fieldsUser[2]}${fieldsUser[3]}${fieldsUser[4]}`;
-    totalUser += parseInt(generate);
-    let elementGenerate = document.createElement('pre');
-    elementGenerate.innerHTML = `#${(i + 1)}   -   ${generate}`;
-
-    //Validate the rank values
-    if (parseInt(generate) < rankUser[0][1]) {
-      rankUser[0][1] = parseInt(generate);
-      rankUser[0][0] = i + 1;
-      document.querySelector('#pnlGameUser .result-min').innerHTML = `#${i + 1} - ${parseInt(generate)}`;
-    }
-    else if (parseInt(generate) > rankUser[1][1]) {
-      rankUser[1][1] = parseInt(generate);
-      rankUser[1][0] = i + 1;
-      document.querySelector('#pnlGameUser .result-max').innerHTML = `#${i + 1} - ${parseInt(generate)}`;
-    }
-
-    //Modify the value estimate
-    document.querySelector('#pnlGameUser .result-obtenido').innerHTML = totalRandom / ((i + 1) * 5);
-
-    //Modify the value average
-    document.querySelector('#pnlGameUser #pnlGameUserPromedio h4').innerHTML = totalUser / (i + 1);
-
-    //Send cycle to list HTML for User
-    var user = document.getElementById('user');
-    user.prepend(elementGenerate);
-
-    if (check) {
-      //Print value for PC
-      let generatePC = `${fieldsPC[0]}${fieldsPC[1]}${fieldsPC[2]}${fieldsPC[3]}${fieldsPC[4]}`;
-      totalPC += parseInt(generatePC);
-      let elementGeneratePC = document.createElement('pre');
-      elementGeneratePC.innerHTML = `#${(i + 1)}   -   ${generatePC}`;
+      //Print value for User
+      let generate = `${fieldsUser[0]}${fieldsUser[1]}${fieldsUser[2]}${fieldsUser[3]}${fieldsUser[4]}`;
+      totalUser += parseInt(generate);
+      let elementGenerate = document.createElement('pre');
+      elementGenerate.innerHTML = `#${(i + 1)}   -   ${generate}`;
 
       //Validate the rank values
-      if (parseInt(generatePC) < rankPC[0][1]) {
-        rankPC[0][1] = parseInt(generatePC);
-        rankPC[0][0] = i + 1;
-        document.querySelector('#pnlGamePC .result-min').innerHTML = `#${i + 1} - ${parseInt(generatePC)}`;
+      if (parseInt(generate) < rankUser[0][1]) {
+        rankUser[0][1] = parseInt(generate);
+        rankUser[0][0] = i + 1;
+        document.querySelector('#pnlGameUser .result-min').innerHTML = `#${i + 1} - ${parseInt(generate)}`;
       }
-      else if (parseInt(generatePC) > rankPC[1][1]) {
-        rankPC[1][1] = parseInt(generatePC);
-        rankPC[1][0] = i + 1;
-        document.querySelector('#pnlGamePC .result-max').innerHTML = `#${i + 1} - ${parseInt(generatePC)}`;
+      else if (parseInt(generate) > rankUser[1][1]) {
+        rankUser[1][1] = parseInt(generate);
+        rankUser[1][0] = i + 1;
+        document.querySelector('#pnlGameUser .result-max').innerHTML = `#${i + 1} - ${parseInt(generate)}`;
       }
 
       //Modify the value estimate
-      document.querySelector('#pnlGamePC .result-obtenido').innerHTML = totalRandom / ((i + 1) * 5);
+      document.querySelector('#pnlGameUser .result-obtenido').innerHTML = totalRandom / ((i + 1) * 5);
 
       //Modify the value average
-      document.querySelector('#pnlGamePC #pnlGamePCPromedio h4').innerHTML = totalPC / (i + 1);
+      document.querySelector('#pnlGameUser #pnlGameUserPromedio h4').innerHTML = totalUser / (i + 1);
 
-      //Send cycle to list HTML for PC
-      var pc = document.getElementById('pc');
-      pc.prepend(elementGeneratePC);
+      //Send cycle to list HTML for User
+      var user = document.getElementById('user');
+      user.prepend(elementGenerate);
 
-      //Calculate versus User - PC
-      if (generate > generatePC)
-        document.querySelector('#versusUser h4').innerHTML = parseInt(document.querySelector('#versusUser h4').textContent) + 1;
-      else if (generate < generatePC)
-        document.querySelector('#versusPC h4').innerHTML = parseInt(document.querySelector('#versusPC h4').textContent) + 1;
-      else
-        document.querySelector('#versusDraw h4').innerHTML = parseInt(document.querySelector('#versusDraw h4').textContent) + 1;
-    }
-    await wait(0);
+      if (check) {
+        //Print value for PC
+        let generatePC = `${fieldsPC[0]}${fieldsPC[1]}${fieldsPC[2]}${fieldsPC[3]}${fieldsPC[4]}`;
+        totalPC += parseInt(generatePC);
+        let elementGeneratePC = document.createElement('pre');
+        elementGeneratePC.innerHTML = `#${(i + 1)}   -   ${generatePC}`;
+
+        //Validate the rank values
+        if (parseInt(generatePC) < rankPC[0][1]) {
+          rankPC[0][1] = parseInt(generatePC);
+          rankPC[0][0] = i + 1;
+          document.querySelector('#pnlGamePC .result-min').innerHTML = `#${i + 1} - ${parseInt(generatePC)}`;
+        }
+        else if (parseInt(generatePC) > rankPC[1][1]) {
+          rankPC[1][1] = parseInt(generatePC);
+          rankPC[1][0] = i + 1;
+          document.querySelector('#pnlGamePC .result-max').innerHTML = `#${i + 1} - ${parseInt(generatePC)}`;
+        }
+
+        //Modify the value estimate
+        document.querySelector('#pnlGamePC .result-obtenido').innerHTML = totalRandom / ((i + 1) * 5);
+
+        //Modify the value average
+        document.querySelector('#pnlGamePC #pnlGamePCPromedio h4').innerHTML = totalPC / (i + 1);
+
+        //Send cycle to list HTML for PC
+        var pc = document.getElementById('pc');
+        pc.prepend(elementGeneratePC);
+
+        //Calculate versus User - PC
+        if (generate > generatePC)
+          document.querySelector('#versusUser h4').innerHTML = parseInt(document.querySelector('#versusUser h4').textContent) + 1;
+        else if (generate < generatePC)
+          document.querySelector('#versusPC h4').innerHTML = parseInt(document.querySelector('#versusPC h4').textContent) + 1;
+        else
+          document.querySelector('#versusDraw h4').innerHTML = parseInt(document.querySelector('#versusDraw h4').textContent) + 1;
+      }
+    }, 50);
   }
-
-  console.log(totalUser, totalUser / values.cycle)
-  // console.log(rankUser, rankPC)
-  console.log(totalRandom, totalRandom / (values.cycle * 5))
 }
 
 //Logical function with the process game
@@ -136,13 +133,4 @@ function processSimulate() {
       fieldsPC[fieldsEmptyPC[valuesPC[rand][j] - 1]] = rand;
     }
   }
-}
-
-//Function promise for timeout
-function wait(time) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve();
-    }, time);
-  });
 }
